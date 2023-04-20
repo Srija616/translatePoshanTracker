ffca4a327cd30cebcd928d103b04a0d09a8a032f
Srija Anand <srija17199@iiitd.ac.in>
2023-04-21 00:17:30 +0530

    Update web_scraper.py - Minor improvements and docstring added.

--- web_scraper.py
+++ web_scraper.py
@@ -1,4 +1,3 @@
-# Focus - get all data  
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
@@ -14,16 +13,14 @@ import config
 import utils
 from bs4 import BeautifulSoup
 import time
-# Add logging
 from indicnlp.tokenize import sentence_tokenize
 import pandas as pd
 import utils
 from utils import tokenizeSentencewise
-
+# Add logging
 INDIC = ["as", "bn", "gu", "hi", "kn", "ml", "mr", "or", "pa", "ta", "te"]  # list of indic languages part of IndicTrans library
 
-# instead of string, make a list
-def split_sentences(paragraph, language):
+def split_sentences(paragraph, language):  # why using this when mosesTokenizer is used. moreover calling tokenizeSentenceWise, makes no sense, will fix.
     if language == "en":
         return tokenizeSentencewise(paragraph, language)
     elif language in INDIC:
@@ -43,7 +40,7 @@ def extract_text_data(driver, url, visited_urls, url_main):
     Arguments:
     driver: Initialized Chrome Webdriver
     url (string): The url of webpage to be scraped
-    visited_urls (list): List of urls already scraped for a given language
+    visited_urls (set): Set of urls already scraped for a given language
     all_text (string): Contains all the text scraped for webpages. 
     
     Output:
@@ -85,36 +82,41 @@ def get_language_driver(language, driver, url):
     Output
     driver: Returns the driver to the website
     '''    
-    # driver.get(url)
-    # driver.implicitly_wait(10)
-    # dropdown = driver.find_element('xpath',"//div[@class='language-dropdown dropdown-menu']") # find the dropdown menu
-    # driver.execute_script("arguments[0].style.display = 'block';", dropdown) # bring the dropdown menu on page
-    # driver.implicitly_wait(10)
-    # dropdown.click() # click the dropdown menu
-    # # Wait for the option to be clickable
-    # option = WebDriverWait(driver, 10).until(
-    #     EC.element_to_be_clickable((By.XPATH, "//button[text()='{}']".format(language)))  # get the option to be clicked
-    # )
-    # option.click() # click the option, the website in required language will open up.
-    try:
-        response = driver.get(url)
-        driver.implicitly_wait(10)
-        element = driver.find_element('xpath',"//div[@class='language-dropdown dropdown-menu']")
-        driver.execute_script("arguments[0].style.display = 'block';", element)  # bring the element of dropdown-menu visible
-        driver.implicitly_wait(10)
-        button = driver.find_element('xpath',"//button[text()='{}']".format(language))
-        driver.implicitly_wait(10)
-        ActionChains(driver).move_to_element(button).click(button).perform()
-        driver.implicitly_wait(10)
-        return driver
-    except Exception as e:
-        print (e)
-        print (response)
-        return
-        
-        
-     
+
+    driver.get(url)
+    driver.implicitly_wait(10)
+    dropdown = driver.find_element('xpath',"//div[@class='language-dropdown dropdown-menu']") # find the dropdown menu
+    driver.execute_script("arguments[0].style.display = 'block';", dropdown) # bring the dropdown menu on page
+    driver.implicitly_wait(10)
+    dropdown.click() # click the dropdown menu
+    # Wait for the option to be clickable
+    option = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//button[text()='{}']".format(language)))  # get the option to be clicked
+    )
+    option.click() # click the option, the website in required language will open up.    
+
+
 def process_language(args):
+    '''
+    Processes data for each language in the following steps:
+    1. Extract Data
+    2. If the language is supported by Indic-NLP Library, split the text into sentences using the split_sentence function
+    3. Detect the language of text using fasttext-detect library.
+    4. Now we tackle data in three different ways:
+        a. If the language is supported by fasttext-detect library, remove data in the text from other languages, remove numeric strings, 
+            and remove duplicates. Save this monolingual data.
+        b. If the language is not supported by fasttext-detec library, removing data from other languages can be quite inaccurate, 
+            therefore only numeric strings and duplicates are removed.
+        c. If the language is common to both our models (indicTrans and opus-mt-mul-en),
+            we have kept a copy (split_text), without any transformations which is directly used for creating the parallel data
+    Arguments:
+    language (string): The language for website to be scraped.
+    driver (ChromeDriver): To interact with the browser
+    url (string): The url of website to be scraped
+    
+    Output
+    driver (ChromeDriver): Returns the driver to the website
+    '''   
     lang, language_isocode, url, url_main = args
     options = Options()
     options.add_experimental_option('excludeSwitches', ['enable-logging']) # to remove a USB related warning message
@@ -130,17 +132,17 @@ def process_language(args):
     all_text = extract_text_data(driver, url, visited_urls, url_main) # all_text is a string
     driver.quit()
     
-    split_text = all_text.split("\n") # split_text is a list of strings - used for translation after some manual cleaning
+     # split_text is a list of strings - used for translation after processing and cleaning (automated and manual)
+    split_text = all_text.split("\n")
     
-    # there are 22 languages on the website but not all are supported by indicTrans and m2m100 for translation.
+    # there are 22 languages on the website, but not all are supported by indicTrans and helsinki-nlp/opus-mt-mul-en for translation.
     # split_sentences is part of indic-nlp library used to split sentences for indic languages
     if language_isocode in config.common_supported_languages:
-        split_text_new = split_sentences(all_text, language_isocode) # don't use it normally, causes issue in alignment
+        split_text_new = split_sentences(all_text, language_isocode)
     else:
         split_text_new = split_text
 
-    detected_language = utils.detectLanguage(all_text.replace("\n", ""))
-    print (f"language detection of {language_isocode}, detected as {detected_language}, len(split_text) = {len(split_text)}")
+    detected_language = utils.detectLanguage(all_text.replace("\n", "")) # fasttext-detect cannot take newline charaters.
     
     if (language_isocode in config.ft_lang_supported): # supported by fastText because we use the detect function
         text_list = utils.removeOtherLanguages(split_text_new, language_isocode)
@@ -191,12 +193,11 @@ def get_all_data(url):
                 df = pd.concat([df_en, df_temp], axis=1)
                 file = "en_" + languages[i] + ".csv"
                 directory = "rawBinaryData"
-                file_path = os.path.join(directory, file)
-                df.to_csv(file_path, index=False)
+                save_data(directory, file, df)
 
 
 if __name__ == "__main__":
-    # can add user argument here to run with the requried webpage
+    # can add user argument here to run with the required webpage
     url_main = "https://www.poshantracker.in/"
     get_all_data(url_main)
     
